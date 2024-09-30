028f592edf39b4bc599aa0ab4ccd4cbf7ba01a29
Ayush8794 <ayushjindal403@gmail.com>
2024-09-30 18:56:44 +0530

    more registration pages added

--- landingpage.js
+++ landingpage.js
@@ -1,7 +1,22 @@
-function openNavbar() {
-  document.getElementById("sideNavigationBar").style.width = "50%";
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+const totalSlides = slides.length;
+
+function showSlide(index) {
+  slides.forEach((slide, i) => {
+    slide.classList.remove('active');
+    if (i === index) {
+      slide.classList.add('active');
+    }
+  });
+
+  const sliderWidth = document.querySelector('.slider').clientWidth;
+  document.querySelector('.slides').style.transform = `translateX(-${index * sliderWidth}px)`;
 }
 
-function closeNavbar() {
-  document.getElementById("sideNavigationBar").style.width = "0%";
+function nextSlide() {
+  currentSlide = (currentSlide + 1) % totalSlides;
+  showSlide(currentSlide);
 }
+
+setInterval(nextSlide, 3000); 
